bd76ea40cf20bd9a67ecc5bf2949f33a2ea87521
BilgiHazinesi <z.dindaroglu@nesibeaydin.k12.tr>
2026-01-09 15:28:17 +0300

    Update script.js

--- Sinav/script.js
+++ Sinav/script.js
@@ -1,5 +1,5 @@
 // *** BURAYA YENİ APPS SCRIPT LİNKİNİZİ YAPIŞTIRIN ***
-const API_URL = "https://script.google.com/macros/s/AKfycbwNlpX6P49zLL3e-BnLRfmD17Y8RzOGPx2SCoJ9h1ngs0_Tkfq0FHT_9Nwh280sLQyp/exec"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbyDVlMuYaZ3-lM_NzPg4bW1DS9lqu1xOt2Pu8-sNXKw1fMnymE_C3VLtjjGNJKmVShN/exec"; 
 
 let currentUser = {};
 let teacherData = { exams: [], students: [], results: [] };
